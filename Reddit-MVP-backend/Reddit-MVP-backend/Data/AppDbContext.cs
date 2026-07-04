42a4a6a6895bf854500cd9ee02526cb478ed83f3
Amir Arabi <115399531+20Amir04@users.noreply.github.com>
2026-07-04 13:23:56 -0700

    Configure EF Core and SQL Server DB

---
+++ Reddit-MVP-backend/Reddit-MVP-backend/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reddit_MVP_backend.Models;
+
+namespace Reddit_MVP_backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options) :  base(options)
+        {
+                
+        }
+
+        public DbSet<TestEntity> TestEntities  { get; set; } 
+    }
+}
---
+++ Reddit-MVP-backend/Reddit-MVP-backend/Models/TestEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Reddit_MVP_backend.Models;
+
+public class TestEntity
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+}
