9f15dd723f290200b8b59726b89193c426e51f64
Amir Arabi <115399531+20Amir04@users.noreply.github.com>
2026-07-06 09:58:29 -0700

    Connect frontend and backend API

---
+++ Reddit-MVP-backend/Reddit-MVP-backend/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Reddit_MVP_backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetHealth()
+    {
+        return Ok(new
+        {
+            status = "API is running"
+        });
+    }
+}
--- Reddit-MVP-backend/Reddit-MVP-backend/Program.cs
+++ Reddit-MVP-backend/Reddit-MVP-backend/Program.cs
@@ -12,8 +12,20 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors("AllowFrontend");
 
 app.UseHttpsRedirection();
 
